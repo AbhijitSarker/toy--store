c12dd18f11d60505cf1a6729a0a12bf32c5ec410
AbhijitSarker <abhijitsarker03@gmail.com>
2023-09-19 15:28:32 +0600

    responsive navbar

--- src/pages/shared/NavBar/NavBar.jsx
+++ src/pages/shared/NavBar/NavBar.jsx
@@ -1,11 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { GiCrossedBones } from 'react-icons/gi';
+import { FaBarsStaggered } from "react-icons/fa6";
+import { Link } from 'react-router-dom';
 
-const NavBar = () => {
+
+const Navbar = () => {
+    const [toggle, setToggle] = useState(false);
+    const navLinks = <>
+        <Link className='hover:bg-[#d2d6e0] px-2 py-1 rounded'><li>Home</li></Link>
+        <Link className='hover:bg-[#d2d6e0] px-2 py-1 rounded'>Store</Link>
+        <Link className='hover:bg-[#d2d6e0] px-2 py-1 rounded'>About</Link>
+    </>
     return (
-        <div>
-            nav
-        </div>
+        <nav className='w-full flex py-2 justify-between items-center text-[#302a3c] font-serif font-semibold shadow-lg bg-[#e8edfc]'>
+
+            <div className='container mx-auto flex  '>
+                <Link><h1 className='text-3xl font-extrabold'>TOY STORE</h1></Link>
+
+                <ul className=' sm:flex hidden justify-center items-center gap-5 text-2xl flex-1'>
+                    {navLinks}
+                </ul>
+                <button className='px-3 py-1 text-2xl rounded-md text-white bg-[#3d91c2] hover:bg-[#31749b] ml-auto transition ease-in-out duration-500'>Login</button>
+            </div>
+
+            <div className='sm:hidden flex flex-1 justify-end  items-center sidebar'>
+
+                <div onClick={() => setToggle((prev) => !prev)}>
+                    {toggle ? <GiCrossedBones className='w-6  h-6 ml-2' /> : <FaBarsStaggered className='w-6 h-6 ml-2' />}
+                </div>
+
+                <div className={`${toggle ? 'flex' : 'hidden'} p-6 absolute top-10 bg-[#e8edfc] right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar`}>
+                    <ul className=' flex flex-col justify-end items-center flex-1'>
+                        {navLinks}
+                    </ul>
+                </div>
+
+            </div>
+        </nav>
     );
 };
 
-export default NavBar;
+export default Navbar;
